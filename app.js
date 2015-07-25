7916e1e790a73979ca9d39c734f54ae217e1fdf0
Mark K <k.mark.kr@gmail.com>
2015-07-25 18:18:42 +0900

    1-3 Built-in Directives. close #3

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 (function () {
     var app = angular.module('store', []);
     app.controller('StoreController', function () {
-        this.product = gem;
+        this.products = gems;
     });
-    var gem = {
-        name: 'Mark',
-        price: 500.5,
-        desc: 'the world most powerful great person!!!'
-    }
+    var gems = [
+        {
+            name: 'Mark1',
+            price: 500.5,
+            desc: 'the world most powerful great person!!!',
+            canPurchase: true,
+            soldOut: true
+        },
+        {
+            name: 'Mark2',
+            price: 505.5,
+            desc: 'the world most handsome person!!!',
+            canPurchase: true,
+            soldOut: false
+        }
+    ]
 })();
