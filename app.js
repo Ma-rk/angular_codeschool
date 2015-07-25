587144be683182270979cce4723ab1cab9ccc046
Mark K <k.mark.kr@gmail.com>
2015-07-25 16:31:55 +0900

    1-2 Index HTML Setup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var app=angular.module('store', [ ]);
+(function () {
+    var app = angular.module('store', []);
+    app.controller('StoreController', function () {
+        this.product = gem;
+    });
+    var gem = {
+        name: 'Mark',
+        price: 500.5,
+        desc: 'the world most powerful great person!!!'
+    }
+})();
